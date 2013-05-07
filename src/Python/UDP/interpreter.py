c720ce26510047dc35b14f022bce5e86aebadb0d
abbad <ahm.abbad@gmail.com>
2013-05-07 16:39:14 +0300

    new module added

---
+++ src/Python/UDP/interpreter.py
@@ -0,0 +1,15 @@
+'''
+Created on May 7, 2013
+
+@author: Abbad
+'''
+
+'''
+	this is for interpreting the packet
+'''
+
+def getPacketSequenceNumber(packet):
+	'''
+		first 32 bytes are the sequence number
+	'''
+	return int(data[0:32], 2)
