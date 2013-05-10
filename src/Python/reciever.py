b0738af38b0b9e528f480ba2fd479f3212526d52
abbad <ahm.abbad@gmail.com>
2013-05-11 00:07:16 +0300

    pipe added to module

--- src/Python/reciever.py
+++ src/Python/reciever.py
@@ -7,19 +7,26 @@ This module will call UDP server and TCP server.
 
 '''
 
-from subprocess import Popen
+from subprocess import Popen, PIPE
 from time import sleep
-from os import system
+from os import system, path, pipe, fdopen
+
+def get_reader_writer():
+    fd_read, fd_write = pipe()
+    return fdopen(fd_read, 'r'), fdopen(fd_write, 'w')
 
 if __name__ == '__main__':
 	
+	# init reader and writer. 
+	reader, writer = get_reader_writer()
+	
 	print 'Starting TCP client'
 	args = ["python", "TCP\TCPClient.py"]
-	p1 = Popen(args, shell=False)
+	p1 = Popen(args, stdin= PIPE, stdout= PIPE, shell=False)
 	
 	print 'Starting UDP server'
 	args =  ["python", "UDP\UDPserver.py", "-n 5"]
-	p2 = Popen(args, shell=False)
+	p2 = Popen(args, stdin= PIPE, stdout= PIPE, shell=False)
 	
 	try:
 		system('pause')  #windows, doesn't require enter
