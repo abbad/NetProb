32315f130aef96cb16f25f68003ee18424b219ef
abbad <ahm.abbad@gmail.com>
2013-07-06 17:26:25 +0300

    notification period variable added, code refactored

--- src/Python/TCP/TCPServer.py
+++ src/Python/TCP/TCPServer.py
@@ -10,51 +10,52 @@ import sys, getopt
 host = '127.0.0.1'
 port = 5005
 bufferSize = 1024 
+notificationPeriod = 5
 
 # global variables
 
 def printHelp():
 	print 'This is a TCP Server:'
 	print 'usage:'
-	print '-l localHost \t\t\t default localhost'
 	print '-p port number \t\t\t default 5005'
-	print '-s buffer size \t\t\t default 1024'
-
+	print '-n notification period'
+	
 def checkArguments(argv):
 	try:
-		opts, args = getopt.getopt(argv[1:],"hl:p:s:",["host", "portNumber", "bufferSize"])
+		opts, args = getopt.getopt(argv[1:],"hp:n:",["portNumber", "notificationPeriod"])
 	except getopt.GetoptError:
-		print 'TCPServer.py -l <hostname> -p <port> -s <bufferSize>'
+		print 'TCPServer.py -p <port> -n <notification period>'
 		sys.exit(2)
 	for opt, arg in opts:
 		if opt == '-h':
 			printHelp()
 			sys.exit()
-		elif opt in ('-l'):
-			global host
-			host = arg
 		elif opt in ('-p'):
 			global port 
 			port = int(arg)
-		elif opt in ('-b'):
-			global bufferSize
-			bufferSize = int(arg)
+		elif opt in ('-n'):
+			global notificationPeriod
+			notificationPeriod = int(arg)
 
-   
-checkArguments(sys.argv)
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((host, port))
-print 'server is listening'
-s.listen(1)
+def createConnection():
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind((host, port))
+	print 'server is listening'
+	s.listen(1)
 
 
-conn, addr = s.accept()
-print 'TCP Server: Connection address:', addr
+	conn, addr = s.accept()
+	print 'TCP Server: Connection address:', addr
 
-while 1:
-	data = conn.recv(bufferSize)
-	print "TCP Server: received data:", data
-
+def main():
+	checkArguments(sys.argv)
+	createConnection()
+		
+	while 1:
+		data = conn.recv(bufferSize)
+		print "TCP Server: received data:", data
+		conn.close()
 	
-conn.close()
+if __name__ == '__main__':
+	main()
+	
